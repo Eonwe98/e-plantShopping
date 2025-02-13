1742bd253175f28ca44e27f684f3c5748b6c1d5f
Eonwe98 <146309387+Eonwe98@users.noreply.github.com>
2025-02-12 23:30:17 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,20 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/e-plantShopping",
   plugins: [react()],
+  
+  esbuild: {
+    loader: 'jsx',
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 });
